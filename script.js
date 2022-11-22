c5e081a8f93060b92e44f350df358312cb1189ca
aaneleh <helenakurzgon@gmail.com>
2022-11-22 11:54:41 -0300

    calculos mais praticos

--- script.js
+++ script.js
@@ -11,17 +11,15 @@ const desenhar = (comprimento, largura, fileiras, modelo) => {
     canvas.fillStyle = '#fff';
     canvas.fillRect(0,0, width, height);
 
-    // Achar quantos metros equivale 1px para aproveitar da melhor maneira o espaço
+    // Quantos metros equivale 1px para aproveitar da melhor maneira o espaço?
     // Exemplo: Se o canvas tem 600px de largura, e o usuário insere um máximo de 100m, cada pixel equivalerá 6px, então uma quadra de 20m será um retangulo de 120px;
-    let equi = comprimento > largura ? width / comprimento : width / largura ;
+    let equi = comprimento > largura ? width / comprimento : width / largura;
 
-    //demarca a área dada
-    canvas.strokeStyle = 'black';
-    canvas.strokeRect(width/2 - ((comprimento*equi)/2), height/2 -((largura*equi)/2), comprimento*equi, largura*equi);
+    //Demarca a área dada
     canvas.fillStyle = '#f5f5f5';
     canvas.fillRect(width/2 - ((comprimento*equi)/2), height/2 -((largura*equi)/2), comprimento*equi, largura*equi);
 
-    //desenhar a quadra de acordo com seu modelo
+    //Desenhar a quadra de acordo com seu modelo
     let quadra_comprimento, quadra_largura;
     let quadra_espacamento = 6*equi;
     switch(modelo){
@@ -39,7 +37,6 @@ const desenhar = (comprimento, largura, fileiras, modelo) => {
             quadra_comprimento = 27*equi, quadra_largura = 16*equi;
             break;
     }
-
     let quadra_total_comprimento = quadra_comprimento+quadra_espacamento,
         quadra_total_largura = quadra_largura+quadra_espacamento;
 
@@ -48,25 +45,37 @@ const desenhar = (comprimento, largura, fileiras, modelo) => {
     canvas.fillStyle = '#7aff70';
     canvas.fillRect(width/2 - quadra_comprimento/2, height/2 - quadra_largura/2, quadra_comprimento, quadra_largura);
 
-    //CADEIRAS
+    //Calcular cadeiras
     let cadeira_tamanho = 1.5 * equi;
-
-
+    let cadeira_espaco_min = 3 * equi;
 
     //quantas cadeiras cabem no comprimento da quadra?
-    let quant_cadeiras_horz = Math.floor( (quadra_total_comprimento) / (3*equi));
+    let quant_cadeiras_horz = Math.floor( (quadra_total_comprimento) / (cadeira_espaco_min));
+
     //quanto deveria ser o espaço para cobrir todo o comprimento?
     let cadeira_espaco_1 = (quadra_total_comprimento) / quant_cadeiras_horz;
 
+    //quantas fileiras cabem no máximo no espaço?
+    let fileiras_max_vert = Math.floor(((largura*equi) - quadra_total_largura) / (cadeira_espaco_1*2));
 
-    //quantas fileiras cabem realmente na largura maxima
-    let fileiras_vert = Math.floor(((largura*equi) - quadra_total_largura) / (cadeira_espaco_1*2));
+    //o nº de fileiras passadas exece a quantidade máxima?
+    let fileiras_vert = fileiras > fileiras_max_vert ? fileiras_max_vert : fileiras;
 
-    console.log(fileiras_vert);
+
+    //quantas cadeiras cabem na largura da quadra + as fileiras superiores e inferiores?
+    let quant_cadeiras_vert = Math.floor( (quadra_total_largura + ( (cadeira_espaco_1*fileiras_vert)*2) ) / (cadeira_espaco_min) );
     
-    
+    //quanto deveria ser o espaço para cobrir todo o comprimento?
+    let cadeira_espaco_2 = (quadra_total_largura + ( (cadeira_espaco_1*fileiras_vert)*2)) / quant_cadeiras_vert;
 
-    for(i = 0; i < fileiras && i < fileiras_vert; i++){
+    //quantas fileiras cabem realmente no comprimento maxima?
+    let fileiras_max_horz = ((comprimento*equi) - quadra_total_comprimento) / (cadeira_espaco_2*2);
+
+    //o nº de fileiras passadas exece a quantidade máxima?
+    let fileiras_horz = fileiras > fileiras_max_horz ? fileiras_max_horz : fileiras;
+
+    //Desenhar cadeiras
+    for(i = 0; i < fileiras_vert; i++){
         for(j = 0; j < quant_cadeiras_horz; j++){
             //Parte de cima
             canvas.fillStyle = '#fff';
@@ -80,41 +89,35 @@ const desenhar = (comprimento, largura, fileiras, modelo) => {
             canvas.fillStyle = 'red';
             canvas.fillRect(width/2 - (quadra_total_comprimento)/2 + (cadeira_tamanho/2) + (j*cadeira_espaco_1), height/2 + ((quadra_total_largura)/2) + (cadeira_tamanho/2) + (i*cadeira_espaco_1), cadeira_tamanho, cadeira_tamanho);
         }
+
     }
-
-
-    let fileira_atual = i;
-
-    //quantas cadeiras cabem na largura da quadra + as fileiras superiores e inferiores?
-
-    //se a largura tiver ultrapassado a largura maxima
-    let quant_cadeiras_vert = Math.floor( ( quadra_total_largura + ( (cadeira_espaco_1*fileira_atual)*2) ) / (3*equi));
-    
-    //quanto deveria ser o espaço para cobrir todo o comprimento?
-    let cadeira_espaco_2 = (quadra_total_largura+((cadeira_espaco_1*fileira_atual )*2)) / quant_cadeiras_vert;
-
-    //quantas fileiras cabem realmente no comprimento maxima
-    let fileiras_horz = ((comprimento*equi) - quadra_total_comprimento) / (cadeira_espaco_2*2);
-
-    for(i = 0; i < fileira_atual && i<fileiras_horz; i++){
+    for(i = 0; i < fileiras_horz; i++){
         for(j = 0; j < quant_cadeiras_vert; j++){
             //Parte da esquerda
             canvas.fillStyle = '#fff';
-            canvas.fillRect(width/2 - ((quadra_total_comprimento)/2) - cadeira_espaco_1 - (i*cadeira_espaco_1) , height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileira_atual) + (cadeira_espaco_2*j), cadeira_espaco_2, cadeira_espaco_2);
+            canvas.fillRect(width/2 - ((quadra_total_comprimento)/2) - cadeira_espaco_1 - (i*cadeira_espaco_1) , height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileiras_vert) + (cadeira_espaco_2*j), cadeira_espaco_2, cadeira_espaco_2);
             canvas.fillStyle = 'red';
-            canvas.fillRect(width/2 - ((quadra_total_comprimento)/2) - cadeira_espaco_1 - (i*cadeira_espaco_1) + (cadeira_tamanho/2) , height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileira_atual) + (cadeira_espaco_2*j) + (cadeira_tamanho/2), cadeira_tamanho, cadeira_tamanho);
+            canvas.fillRect(width/2 - ((quadra_total_comprimento)/2) - cadeira_espaco_1 - (i*cadeira_espaco_1) + (cadeira_tamanho/2) , height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileiras_vert) + (cadeira_espaco_2*j) + (cadeira_tamanho/2), cadeira_tamanho, cadeira_tamanho);
             
             //Parte da direita
             canvas.fillStyle = '#fff';
-            canvas.fillRect(width/2 + ((quadra_total_comprimento)/2) + (i*cadeira_espaco_1), height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileira_atual) + (cadeira_espaco_2*j), cadeira_espaco_2, cadeira_espaco_2);
+            canvas.fillRect(width/2 + ((quadra_total_comprimento)/2) + (i*cadeira_espaco_1), height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileiras_vert) + (cadeira_espaco_2*j), cadeira_espaco_2, cadeira_espaco_2);
             canvas.fillStyle = 'red';            
-            canvas.fillRect(width/2 + ((quadra_total_comprimento)/2) + (i*cadeira_espaco_1) + (cadeira_tamanho/2), height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileira_atual) + (cadeira_espaco_2*j) + (cadeira_tamanho/2), cadeira_tamanho, cadeira_tamanho);
+            canvas.fillRect(width/2 + ((quadra_total_comprimento)/2) + (i*cadeira_espaco_1) + (cadeira_tamanho/2), height/2 - ((quadra_total_largura)/2) - (cadeira_espaco_1*fileiras_vert) + (cadeira_espaco_2*j) + (cadeira_tamanho/2), cadeira_tamanho, cadeira_tamanho);
         }
     }
+
+
+
+
+
 }
 
+
 //usa a função ao iniciar
-window.onload = () => { desenhar(comprimento, largura, fileiras, modelo) }
+window.onload = () => {
+    desenhar(comprimento, largura, fileiras, modelo)
+}
 
 //pega valores digitados pelo usuario
 document.getElementById('atualizar').addEventListener('click', () => {
